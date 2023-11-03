7a6ad6b582922ca1771d060fbb886582d5f939a2
mainangaruiya <003paulmaina@gmail.com>
2023-11-03 10:46:53 +0300

    ilter_datum that returns the log message obfuscated:

--- filtered_logger.py
+++ filtered_logger.py
@@ -2,7 +2,13 @@
 """
 module for filtering loggers
 """
+
+import os
 import re
+import logging
+import mysql.connector
+from typing import List
+
 patterns = {
     'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
     'replace': lambda x: r'\g<field>={}'.format(x),
