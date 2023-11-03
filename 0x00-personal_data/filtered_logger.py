6bb6a57d08655bcc1913f627d970f7527ed0e58f
mainangaruiya <003paulmaina@gmail.com>
2023-11-03 12:09:59 +0300

    get_logger function that takes no arguments and returns a logging.Logger object.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -28,3 +28,19 @@ def get_logger() -> logging.Logger:
     logger.Propagate = False
     logger.addHandler(stream_handler)
     return logger
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Creates a connector to a database.
+    """
+    db_host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    b_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+    db_user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    db_pwd = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    connection = mysql.connector.connect(
+            host=db_host,
+            port=3306,
+            user=db_user,
+            password=db_pwd,
+            database=db_name,
+        )
+        return connection
