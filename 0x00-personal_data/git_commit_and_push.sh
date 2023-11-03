71ebcecc0d88c0e54e01c0eacbd2ebcb693ed61a
mainangaruiya <003paulmaina@gmail.com>
2023-11-03 11:24:12 +0300

    fixes on errors

---
+++ 0x00-personal_data/git_commit_and_push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add all changes to the Git staging area
+git add .
+
+# Commit the changes with a specified commit message
+git commit -m 'fixes on errors'
+
+# Push the committed changes to the remote repository
+git push
