fcb8a58e58dee0dd0ca7f0522cdb59e5062b8ce1
mainangaruiya <003paulmaina@gmail.com>
2023-11-03 12:44:51 +0300

    get_logger function that takes no arguments and returns a logging.Logger object.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -67,3 +67,24 @@ def main():
             log_record = logging.LogRecord(*args)
             infor_logger.handle(log_record)
 
+class RedactingFormatter(logging.Formatter):
+    """RedactingFormatter class
+    """
+    REDUCTION = "***"
+    FORMAT ="[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT_FIELDS = ('name', 'levelname', 'asctime', 'message')
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.RedactingFormatter,msg, self.SEPARATOR)
+        return txt
+
+
+
+if __name__ == "__main__"
+    main()
