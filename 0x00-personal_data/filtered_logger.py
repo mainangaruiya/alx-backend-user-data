db936287916367067b4cbee693778e8d8de3b892
mainangaruiya <003paulmaina@gmail.com>
2023-11-03 10:50:38 +0300

    fix

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+module for filtering loggers
+"""
+
+import os
+import re
+import logging
+import mysql.connector
+from typing import List
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
