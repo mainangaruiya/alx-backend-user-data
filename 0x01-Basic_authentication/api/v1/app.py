9b86a0f94b07e59ec280579dfac743486cabd7e4
mainangaruiya <003paulmaina@gmail.com>
2023-11-07 18:48:11 +0300

    HTTP status code for a request unauthorized? 401 of course!

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,16 +2,32 @@
 """
 Route module for the API
 """
+import os
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
+from api.v1.views import app_views
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+auth_type = getenv('AUTH_TYPE' 'auth')
+if auth_type == 'auth':
+    auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
+
+@app.errorhandler(401)
+def not_found(error) -> str:
+    """authorization handler handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(404)
