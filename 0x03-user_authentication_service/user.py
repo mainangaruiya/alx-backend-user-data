699059136c2a00a9713e8391170624cd1f3b3e31
mainangaruiya <003paulmaina@gmail.com>
2023-11-15 11:12:58 +0300

    tasl 1 co,plete

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -5,13 +5,15 @@ model
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
 class User(Base):
 	"""
 	from user table
 	"""
 	__tablename__="users"
-	id = column(Integer, primary_key=True)
-	email = column(String(250), nullable=False)
+	id = Column(Integer, primary_key=True)
+	email = Column(String(250), nullable=False)
 	hashed_password = Column(String(250),nullable=False)
 	session_id = Column(String(250), nullable=True)
 	reset_token = Column(String(250), nullable=True)
